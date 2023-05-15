f832c77b27e043051850d65334a71c5b4288bcfb
Martien van den Akker <martien.van.den.akker.oracle.com>
2023-05-15 11:11:37 +0200

    Solve typos

--- CoolCars/scripts/ref vs deref.sql
+++ CoolCars/scripts/ref vs deref.sql
@@ -2,25 +2,25 @@ declare
  l_cars car_cars_ref_t;
  l_car  car_car_t;
 begin
-  -- Bouw collectie met references op
+  -- Build a collection with references
   select cast(multiset(select make_ref( car_ov_cars
                                       , cae.car_license
                                       )
                        from carsavailable cae) as car_cars_ref_t)
   into l_cars
   from dual;
-  -- Verwerk collection
+  -- Process the collection
   if l_cars.count > 0
   then
     for l_idx in l_cars.first..l_cars.last
     loop
       dbms_output.put_line( 'Car '||l_idx||': ');
-      -- Haal object-value op basis van reference op
-      select deref(l_cars(l_idx))   
+      -- Get the object referred to by the reference
+      select deref(l_cars(l_idx))  
       into l_car
       from dual;
-      -- Druk object af
+      -- Print the object
       l_car.print;
     end loop;
   end if;
-end;                              
+end;
